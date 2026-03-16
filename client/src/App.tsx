5ae4ef2ea60035179e255c9b7db1a6a1db8487dc
aawais2080 <33486337+aawais2080@users.noreply.github.com>
2026-03-16 09:23:56 +0000

    project details slug fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ function Router() {
         <Route path="/services" component={Services} />
         <Route path="/our-work" component={OurWork} />
         {/* <Route path="/project/:id" component={ProjectDetail} /> */}
-        <Route path="/our-work/:id" component={ProjectDetail} />
+        <Route path="/our-work/:slug" component={ProjectDetail} />
         <Route path="/team" component={Team} />
         <Route path="/process" component={Process} />
         <Route path="/contact" component={Contact} />
